fc382b6327f61309360a3d2375a82fd9ee976c26
balaji049 <049balaji@gmail.com>
2026-02-11 22:50:04 +0530

    Remove unused files and clean project structure

--- server.js
+++ server.js
@@ -93,7 +93,6 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/community", require("./routes/communityRoutes"));
 app.use("/api/resources", require("./routes/resourceRoutes"));
 app.use("/api/messages", require("./routes/messageRoutes"));
-app.use("/api/livekit", require("./routes/livekitRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/ai", require("./routes/aiRoutes"));
 app.use("/api/search", require("./routes/searchRoutes"));
